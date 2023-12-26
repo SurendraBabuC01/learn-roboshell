2d4fee5da25daefdfb44e06422b38c454deed184
surendra Babu C <surendra9703802865@gmail.com>
2023-12-26 23:36:46 +0530

    First Script

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -8,9 +8,8 @@ resource "aws_instance" "test" {
 }
 
 data "aws_ami" "example" {
-  owners           = "973714476881"
+  owners            = "973714476881"
   most_recent      = true
-  name_regex       = "^myami-\\d{3}"
   }
 
 output "ami_id" {
