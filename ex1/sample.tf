d8caf578c35ca8003ea2b94ab184004952315702
surendra Babu C <surendra9703802865@gmail.com>
2023-12-26 23:34:56 +0530

    First Script

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -7,6 +7,17 @@ resource "aws_instance" "test" {
   }
 }
 
+data "aws_ami" "example" {
+  owners           = "973714476881"
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  }
+
+output "ami_id" {
+  value = data.aws_ami.example.image_id
+}
+
+
 output "private_ip" {
   value = aws_instance.test.private_ip
 }
