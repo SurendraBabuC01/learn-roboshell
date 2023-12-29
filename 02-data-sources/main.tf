71456f3f7f8aec195c106cf81eaf9c1894886548
surendra Babu C <surendra9703802865@gmail.com>
2023-12-29 08:03:00 +0530

    First Script

---
+++ 02-data-sources/main.tf
@@ -0,0 +1,11 @@
+variable "security_group_name" {
+  default = "allow-all"
+}
+
+data "aws_security_group" "selected" {
+  name = var.security_group_name
+}
+
+output "security_group" {
+  value = data.aws_security_group.selected
+}
