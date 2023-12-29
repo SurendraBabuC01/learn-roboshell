6a4c80e2b1a8e7669cb012030d3a9eeb4f962699
surendra Babu C <surendra9703802865@gmail.com>
2023-12-29 08:39:30 +0530

    First Script

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "null" {
   count = 10
 }
+
+variable "fruits" {
+  default = ["apple", "mango", "banana"]
+}
+
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+}
