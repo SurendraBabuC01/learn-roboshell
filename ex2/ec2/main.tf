b6c1dbdfeda4e98905280a38c2671728ada8edfe
surendra Babu C <surendra9703802865@gmail.com>
2024-01-01 20:48:35 +0530

    Terraform

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "centos" {
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["var.sg_id"]
+  vpc_security_group_ids = [var.sg_id]
 
   tags = {
     Name = "test"
