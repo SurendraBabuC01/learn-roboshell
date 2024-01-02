6999f0e171909085d87a5223f1548e646622b0d7
surendra Babu C <surendra9703802865@gmail.com>
2024-01-02 21:27:58 +0530

    Terraform

--- 07-import/main.tf
+++ 07-import/main.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "instance" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+
+  tags = {
+    "Name" = "workstation"
+  }
 }
 
 output "private_ip" {
